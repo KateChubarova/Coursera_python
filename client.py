14dca9a8eb2410069b8db3451527e40c2c159b2f
kate.chubarava <k.chubarava@altoros.com>
2020-10-28 11:54:57 +0300

    server

--- client.py
+++ client.py
@@ -1,58 +1,51 @@
 import socket
 import time
 
-encoding = "utf-8"
-success = "ok\n"
-
 class ClientError(Exception):
     pass
 
-
 class Client:
+    def __init__(self, addr, port, timeout=None):
+        self._addr = addr
+        self._port = int(port)
+        self._timeout = int(timeout)
 
-    def __init__(self, address, port, timeout=0):
-        self.address = address
-        self.port = port
-        self.timeout = timeout
+    def send(self, cmd):
+        with socket.create_connection((self._addr, self._port), self._timeout) as sock:
+            sock.sendall(cmd.encode("utf8"))
+            buf = sock.recv(1024)
+            print(buf)
+            return buf.decode('utf-8')
 
-    def get_data(self, request):
-        with socket.create_connection((self.address, self.port), self.timeout) as s:
-            s.sendall(request.encode(encoding))
-            response = s.recv(1024).decode(encoding)
-            if not response.startswith(success):
-                raise ClientError(response)
-            return response
-
-    def check_metric_valid(self, metric):
-        if len(metric) != 3: return False
-        try:
-            float(metric[1])
-            int(metric[2])
-        except ValueError:
-            return False
-
-        return True
-
-    def put(self, metric, value, timestamp=-1):
-        if timestamp < 0:
-            timestamp = int(time.time())
-        request = 'put ' + metric + ' ' + str(value) + ' ' + str(timestamp) + '\n'
-        self.get_data(request)
-
-    def get(self, metric):
-        request = 'get ' + metric + '\n'
-        response = self.get_data(request)
-        metrics_dict = dict()
-        metrics = response.split('\n')
-        for l in metrics[1:-2]:
+    def get(self, key):
+        resp = self.send('get ' + key + '\n')
+        print(resp)
+        if resp[0:3] != 'ok\n':
+            raise ClientError(resp)
+        ret = dict()
+        lines = resp.split('\n')
+        for l in lines[1:-2]:
             metric = l.split(' ')
-            if not self.check_metric_valid(response):
-                raise ClientError(response)
             res_key = metric[0]
             res_val = float(metric[1])
             res_ts = int(metric[2])
-            if not res_key in metrics_dict:
-                metrics_dict[res_key] = list()
-            metrics_dict[res_key].append((res_ts, res_val))
-            metrics_dict[res_key].sort(key=lambda tup: tup[0])
-        return metrics_dict
+            if not res_key in ret:
+                ret[res_key] = list()
+            ret[res_key].append((res_ts, res_val))
+            ret[res_key].sort(key=lambda tup: tup[0])
+
+        return ret
+
+    def put(self, key, val, timestamp=None):
+        resp = self.send('put ' + key + ' ' + str(val) + ' ' + str(timestamp if timestamp else int(time.time())) + '\n')
+        if resp[0:3] != 'ok\n':
+            raise ClientError(resp)
+
+
+client = Client("127.0.0.1", 8181, 100)
+client.put("test_float_value",42, 1503319345)
+client.put("test_timestamp", 0.0, 1503319740)
+client.put("test_timestamp", 100.99, 1503319740)
+
+client.get("test_float_value")
+client.get("*")
--- solution.py
+++ solution.py
@@ -2,7 +2,7 @@ import asyncio
 
 success = "ok\n"
 error = "error\nwrong command\n\n"
-metrics = {}
+metrics = dict()
 
 
 def run_server(host, port):
@@ -24,42 +24,70 @@ def run_server(host, port):
     loop.close()
 
 
+class Item:
+    def __init__(self, value, timestamp):
+        self.value = float(value)
+        self.timestamp = int(timestamp)
+
+    def __eq__(self, other):
+        return self.timestamp == other.timestamp
+
+
 class ClientServerProtocol(asyncio.Protocol):
+
+    def check_request(self, request):
+        command = request[0]
+        if command == 'get':
+            return len(request) == 2
+        elif command == 'put':
+            if len(request) != 4:
+                return False
+            try:
+                float(request[2])
+                int(request[3])
+            except:
+                return False
+        else:
+            return False
+        return True
 
     def process_data(self, data):
         request = data.rstrip().split(' ')
+        if not self.check_request(request):
+            return error
         command = request[0]
         if command == 'get':
             return self.get(request)
-        elif command == 'put':
+        if command == 'put':
             return self.put(request)
-        else:
-            return error
 
     def get(self, data):
         metric = data[1]
         response = success
         if metric == '*':
-            for key, values in metrics.items():
-                for value in values:
-                    response = response + metric + ' ' + value[1] + ' ' + value[0] + '\n'
+            for key in metrics:
+                for item in metrics[key]:
+                    response = response + key + ' ' + str(item.value) + ' ' + str(item.timestamp) + '\n'
         else:
             if metric in metrics:
-                for value in metrics[metric]:
-                    response = response + metric + ' ' + value[1] + ' ' + value[0] + '\n'
-
-        # print(response)
+                for item in metrics[metric]:
+                    response = response + metric + ' ' + str(item.value) + ' ' + str(item.timestamp) + '\n'
         return response + '\n'
 
     def put(self, data):
         metric = data[1]
         value = data[2]
         timestamp = data[3]
+        item = Item(value, timestamp)
         if not metric in metrics:
             metrics[metric] = list()
-        if not (timestamp, value) in metrics[metric]:
-            metrics[metric].append((timestamp, value))
-            metrics[metric].sort(key=lambda tup: tup[0])
+        if not item in metrics[metric]:
+            metrics[metric].append(item)
+        else:
+            _metric = metrics[metric]
+            index = _metric.index(item)
+            _metric[index] = item
+            metrics[metric] = _metric
         return success + '\n'
 
     def connection_made(self, transport):
@@ -70,4 +98,4 @@ class ClientServerProtocol(asyncio.Protocol):
         self.transport.write(response.encode('utf-8'))
 
 
-# run_server("127.0.0.1", 10003)
+# run_server("127.0.0.1", 8181)
